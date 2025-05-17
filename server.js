b372409eef4d769e3908c3d7f004a88c47793f48
Eddy Hernandez <eddy@solarx.app>
2025-05-17 18:21:09 -0500

    :test_tube:

--- server.js
+++ server.js
@@ -1,73 +1,81 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
-const PORT = process.env.PORT || 5000;
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+const path = require('path');
+const fs = require('fs');
 
-// Serve static files from the React app
-app.use(express.static(path.resolve(__dirname, "./build")));
-
-// Handle Pokemon routes
-app.get("/:id", async (req, res) => {
-  const pokemonId = req.params.id;
-  const filePath = path.resolve(__dirname, "./build", "index.html");
+// Handle Pokemon routes first
+app.get('/:id', async function(request, response) {
+  console.log(`Pokemon #${request.params.id} page visited!`);
+  const filePath = path.resolve(__dirname, './build', 'index.html');
 
   try {
+    console.log('Fetching Pokemon data...');
     // Fetch Pokemon data
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-    const pokemon = await response.json();
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${request.params.id}`);
+    const pokemon = await pokemonResponse.json();
+    console.log('Pokemon data received:', pokemon.name);
 
     // Read the index.html file
-    fs.readFile(filePath, "utf8", (err, data) => {
+    fs.readFile(filePath, 'utf8', function (err, data) {
       if (err) {
+        console.error('Error reading file:', err);
         return console.log(err);
       }
+      console.log('File read successfully');
 
       const title = `#${pokemon.id.toString().padStart(3, '0')} ${pokemon.name}`;
       const description = `View details for ${pokemon.name}, Pokemon #${pokemon.id.toString().padStart(3, '0')}`;
       const image = pokemon.sprites.front_default;
-      const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      const url = `${request.protocol}://${request.get('host')}${request.originalUrl}`;
 
-      // Replace placeholders with actual values
-      data = data
-        .replace(/__TITLE__/g, title)
-        .replace(/__DESCRIPTION__/g, description)
-        .replace(/__IMAGE__/g, image)
-        .replace(/__URL__/g, url);
+      console.log('Replacing meta tags with:', { title, description, image, url });
 
-      res.send(data);
+      // Replace the special strings with server generated strings
+      data = data.replace(/\$OG_TITLE/g, title);
+      data = data.replace(/\$OG_DESCRIPTION/g, description);
+      data = data.replace(/\$OG_IMAGE/g, image);
+      data = data.replace(/\$OG_URL/g, url);
+
+      console.log('Sending response...');
+      response.send(data);
     });
   } catch (error) {
     console.error('Error:', error);
-    // If there's an error, serve the default index.html
-    fs.readFile(filePath, "utf8", (err, data) => {
-      if (err) {
-        return console.log(err);
-      }
-      res.send(data);
-    });
+    response.sendFile(filePath);
   }
 });
 
 // Handle root route
-app.get("/", (req, res) => {
-  const filePath = path.resolve(__dirname, "./build", "index.html");
-  fs.readFile(filePath, "utf8", (err, data) => {
+app.get('/', function(request, response) {
+  console.log('Home page visited!');
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+
+  fs.readFile(filePath, 'utf8', function (err, data) {
     if (err) {
+      console.error('Error reading file:', err);
       return console.log(err);
     }
+    console.log('File read successfully');
 
-    data = data
-      .replace(/__TITLE__/g, "Pokemon PWA")
-      .replace(/__DESCRIPTION__/g, "A Progressive Web App for Pokemon enthusiasts")
-      .replace(/__IMAGE__/g, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
-      .replace(/__URL__/g, `${req.protocol}://${req.get('host')}`);
+    data = data.replace(/\$OG_TITLE/g, 'Pokemon PWA');
+    data = data.replace(/\$OG_DESCRIPTION/g, 'A Progressive Web App for Pokemon enthusiasts');
+    data = data.replace(/\$OG_IMAGE/g, 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png');
+    data = data.replace(/\$OG_URL/g, `${request.protocol}://${request.get('host')}`);
 
-    res.send(data);
+    console.log('Sending response...');
+    response.send(data);
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-}); 
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, './build')));
+
+// Handle all other routes
+app.get('*', function(request, response) {
+  console.log('Catch-all route hit:', request.path);
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  response.sendFile(filePath);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
